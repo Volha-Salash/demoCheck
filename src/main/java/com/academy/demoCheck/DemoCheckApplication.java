010a14ccddede10e20c64768e7083574a4617bd7
Volha Salash <99347089+kursonchik@users.noreply.github.com>
2022-12-14 12:40:25 +0300

    Initial commit

---
+++ src/main/java/com/academy/demoCheck/DemoCheckApplication.java
@@ -0,0 +1,13 @@
+package com.academy.demoCheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academy/demoCheck/DemoCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.demoCheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
